a950a3b001aaa436c13fd80969a29fd0e93b24f0
potatoyum <46017398+potatoyum@users.noreply.github.com>
2019-06-18 17:14:28 +0900

    1차-미완성
    메인스크린에 정산화면 연결

---
+++ src/Menu.java
@@ -0,0 +1,57 @@
+package restaurant;
+
+
+
+public class Menu {
+	String name;
+	int cost;
+	int sold;
+	int cogs; //Cost of goods sold (매출원가)
+	
+	public Menu(String name, int cost,int cogs)
+	{
+		this.name= name;
+		this.cost = cost;
+		this.sold = 0;
+		this.cogs = cogs;
+	}
+	
+	public void sold_reset() {
+		this.sold = 0;
+	}
+	
+	
+}
+
+class MakeMenu {
+	String menuName[] = {"불고기버거", "치킨버거","치즈버거","통새우버거","한우버거","강정버거",
+			 "감자튀김","아이스크림","스낵랩",
+			 "콜라", "사이다", "환타","마운틴듀"};
+				
+	int menuCost[] = {2000,3000,2000,4000,4500,3500,
+		1500,500,2500,
+		1000,1000,1000,1000};
+	
+	int menuCogs[] = {1060,1680,1060,2240,2520,1960,840,280,1400,250,250,250,250};
+	
+	public static Menu[] menu = new Menu[13];
+	
+	public void create() {
+		
+		for(int i = 0; i<menu.length; i++)
+		{
+			menu[i] = new Menu(menuName[i], menuCost[i],menuCogs[i]);
+			menu[i].sold=0;
+		}
+	}
+	
+	public void printMenu() {
+		for(int i = 0; i<menu.length; i++)
+		{
+			System.out.printf("%d : %s, %d \n",i,menu[i].name,menu[i].cost);
+		}
+	}
+	
+
+}
+
---
+++ src/Sales.java
@@ -0,0 +1,324 @@
+package restaurant;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import java.util.Arrays;
+import java.util.Vector;
+import java.awt.Font;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Color;
+
+@SuppressWarnings("serial")
+public class Sales extends JFrame {
+
+	//private JPanel contentPane;
+	JPanel p2;
+	/**
+	 * Launch the application.
+	 */
+	
+	/**
+	 * Create the frame.
+	 */
+	public Sales() {			
+			
+		p2 = new JPanel();
+			//Order order = new Order();
+		String header1[]= {"상품명","수량","금액"};
+
+//		매출 순이익 확인 테이블
+		DefaultTableModel model1 = new DefaultTableModel(header1,0);
+		JTable stable = new JTable(model1);
+		int totalsales=0;
+		int profitsales=0;
+		int pprofit=0;
+		stable.setRowHeight(30);
+		
+		for(int i = 0; i<MakeMenu.menu.length;i++) {
+			System.out.printf("%d,",MakeMenu.menu[i].sold);
+			if(MakeMenu.menu[i].sold!=0) {
+				Vector<Object> contents1 = new Vector<Object>();
+				contents1.addElement(MakeMenu.menu[i].name);
+				contents1.addElement(MakeMenu.menu[i].sold);
+				contents1.addElement(MakeMenu.menu[i].cost*MakeMenu.menu[i].sold);
+				totalsales += MakeMenu.menu[i].cost*MakeMenu.menu[i].sold;
+				profitsales += (MakeMenu.menu[i].cogs*MakeMenu.menu[i].sold);
+				model1.addRow(contents1);
+			}
+			pprofit = totalsales - profitsales;
+		}
+		
+		String totalheader[]= {"총 매출"};
+		DefaultTableModel totalmodel = new DefaultTableModel(totalheader,0);
+		Vector<Object> total = new Vector<Object>();
+		total.addElement(totalsales);
+		totalmodel.addRow(total);
+		JTable totaltable = new JTable(totalmodel);
+		
+		String profitheader[]= {"순이익"};
+		DefaultTableModel profitmodel = new DefaultTableModel(profitheader,0);
+		Vector<Object> profit = new Vector<Object>();
+		profit.addElement(pprofit);
+		profitmodel.addRow(profit);
+		JTable profittable = new JTable(profitmodel);
+		
+		  TableColumnModel columnModel = stable.getColumnModel();
+		  TableColumn column0 = columnModel.getColumn(0); 
+	      TableColumn column1 = columnModel.getColumn(1); 
+	      TableColumn column2 = columnModel.getColumn(2); 
+	      DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+	      DefaultTableCellRenderer renderer1 = new DefaultTableCellRenderer();
+	      renderer.setHorizontalAlignment(JLabel.RIGHT);
+	      renderer1.setHorizontalAlignment(JLabel.CENTER);
+	      column0.setCellRenderer(renderer1);
+	      column1.setCellRenderer(renderer1);
+	      column2.setCellRenderer(renderer1);
+	      
+	      TableColumnModel columntotalModel = totaltable.getColumnModel();
+		  TableColumn tcolumn = columntotalModel.getColumn(0); 
+	      tcolumn.setCellRenderer(renderer1);
+		      
+	      TableColumnModel columnprofitModel = profittable.getColumnModel();
+		  TableColumn pcolumn = columnprofitModel.getColumn(0); 
+	      pcolumn.setCellRenderer(renderer1);
+		
+		String mainrank[]= {"순위","수량","주메뉴"};
+		DefaultTableModel model2 = new DefaultTableModel(mainrank,0);
+		JTable maintable = new JTable(model2);
+		maintable.setFont(new Font("굴림", Font.PLAIN, 13));
+		maintable.setRowHeight(30);
+//		순위 매기기 소팅
+		sort(0,5);
+		int cnt1=1;
+		for(int i = 0; i<=5;i++) {
+			if(MakeMenu.menu[i].sold!=0) {
+				if(cnt1==3)break;
+				Vector<Object> maincontents = new Vector<Object>();
+				maincontents.addElement(cnt1);
+				maincontents.addElement(MakeMenu.menu[i].sold);
+				maincontents.addElement(MakeMenu.menu[i].name);	
+				model2.addRow(maincontents);
+			}
+			cnt1++;
+		}
+		
+		String siderank[]= {"순위","수량","사이드메뉴"};
+		DefaultTableModel model3 = new DefaultTableModel(siderank,0);
+		JTable sidetable = new JTable(model3);
+		sidetable.setFont(new Font("굴림", Font.PLAIN, 13));
+		sidetable.setRowHeight(30);
+//		순위 매기기 소팅
+		sort(6,8);
+		int cnt2=1;
+		for(int i = 6; i<=8;i++) {
+			if(MakeMenu.menu[i].sold!=0) {
+				if(cnt2==3)break;
+				Vector<Object> sidecontents = new Vector<Object>();
+				sidecontents.addElement(cnt2);
+				sidecontents.addElement(MakeMenu.menu[i].sold);
+				sidecontents.addElement(MakeMenu.menu[i].name);
+				model3.addRow(sidecontents);
+			}
+			cnt2++;
+		}		
+		
+		String drinkrank[]= {"순위","수량","음료"};
+		DefaultTableModel model4 = new DefaultTableModel(drinkrank,0);
+		JTable drinktable = new JTable(model4);
+		drinktable.setFont(new Font("굴림", Font.PLAIN, 13));
+		drinktable.setRowHeight(30);
+//		순위 매기기 소팅
+		sort(9,12);
+		int cnt3=1;
+		
+		for(int i = 9; i<=12;i++) {
+			if(MakeMenu.menu[i].sold!=0) {
+				if(cnt3==3) break;
+				Vector<Object> drinkcontents= new Vector<Object>();
+				drinkcontents.addElement(cnt3);
+				drinkcontents.addElement(MakeMenu.menu[i].sold);
+				drinkcontents.addElement(MakeMenu.menu[i].name);
+				model4.addRow(drinkcontents);
+			}
+			cnt3++;
+		}
+		
+		  TableColumnModel mcolumnModel = maintable.getColumnModel();
+		  TableColumn mcolumn0 = mcolumnModel.getColumn(0); 
+	      TableColumn mcolumn1 = mcolumnModel.getColumn(1); 
+	      TableColumn mcolumn2 = mcolumnModel.getColumn(2); 
+	      mcolumn0.setCellRenderer(renderer);
+	      mcolumn1.setCellRenderer(renderer);
+	      mcolumn2.setCellRenderer(renderer);
+	      
+	      TableColumnModel scolumnModel = sidetable.getColumnModel();
+		  TableColumn scolumn0 = scolumnModel.getColumn(0); 
+	      TableColumn scolumn1 = scolumnModel.getColumn(1); 
+	      TableColumn scolumn2 = scolumnModel.getColumn(2); 
+	      scolumn0.setCellRenderer(renderer);
+	      scolumn1.setCellRenderer(renderer);
+	      scolumn2.setCellRenderer(renderer);
+	      
+	      TableColumnModel dcolumnModel = drinktable.getColumnModel();
+		  TableColumn dcolumn0 = dcolumnModel.getColumn(0); 
+	      TableColumn dcolumn1 = dcolumnModel.getColumn(1); 
+	      TableColumn dcolumn2 = dcolumnModel.getColumn(2); 
+	      dcolumn0.setCellRenderer(renderer);
+	      dcolumn1.setCellRenderer(renderer);
+	      dcolumn2.setCellRenderer(renderer);
+	      
+	      
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setBounds(100, 100, 1024, 768);
+		//contentPane = new JPanel();
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		//setContentPane(contentPane);
+		//contentPane.setLayout(new CardLayout(0, 0));
+		
+		
+		p2.setBackground(new Color(255, 228, 225));
+		p2.setForeground(Color.BLACK);
+		//contentPane.add(p2, "name_359157631449594");
+		p2.setLayout(null);
+		p2.setBounds(100, 100, 1024, 768);
+		JPanel Contentspanel = new JPanel();
+		Contentspanel.setBounds(0, 0, 996, 534);
+		p2.add(Contentspanel);
+		
+//		content가 들어가는 판은 cardlayout으로 만들거야
+		CardLayout card = new CardLayout(0,0);
+		Contentspanel.setLayout(card);
+		
+		JPanel sales_panel = new JPanel();
+		JPanel ranking_panel = new JPanel();
+		sales_panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane(stable);
+		scrollPane.setBounds(0, 0, 996, 357);
+		sales_panel.add(scrollPane);
+		
+//		p2판은 gridlayout으로 할게
+		ranking_panel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+//		table을 판 위에 올리기
+		JScrollPane scrollPane_1 = new JScrollPane(maintable);
+		ranking_panel.add(scrollPane_1);
+		
+		JScrollPane scrollPane_2 = new JScrollPane(sidetable);
+		ranking_panel.add(scrollPane_2);
+		
+		JScrollPane scrollPane_3 = new JScrollPane(drinktable);
+		ranking_panel.add(scrollPane_3);
+		
+		JLabel label = new JLabel("");
+		ranking_panel.add(label);
+		
+		JPanel Buttonpanel = new JPanel();
+		Buttonpanel.setBounds(0, 535, 996, 75);
+		p2.add(Buttonpanel);
+		
+//		Contentspanel에 panel 추가
+		Contentspanel.add("aa",sales_panel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 360, 996, 162);
+		sales_panel.add(panel);
+		
+		//		table cell 높이 조정
+				totaltable.setRowHeight(30);
+				profittable.setRowHeight(30);
+				panel.setLayout(new GridLayout(0, 1, 0, 0));
+				JScrollPane totalscrollPane = new JScrollPane(totaltable);
+				panel.add(totalscrollPane);
+				
+				JScrollPane profitscrollPane = new JScrollPane(profittable);
+				panel.add(profitscrollPane);
+		Contentspanel.add("bb",ranking_panel);
+		
+//		버튼
+		JButton Sales = new JButton("매출 / 순이익");
+		Sales.setFont(new Font("굴림", Font.PLAIN, 15));
+		Sales.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					card.show(Contentspanel,"aa");
+			}
+		});
+		Buttonpanel.setLayout(new GridLayout(0, 2, 0, 0));
+		Buttonpanel.add(Sales);
+		
+		JButton Ranking = new JButton("판매순위");
+		Ranking.setFont(new Font("굴림", Font.PLAIN, 15));
+		Ranking.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				card.show(Contentspanel,"bb");
+			}
+		});
+		Buttonpanel.add(Ranking);
+		
+		JButton BackButton = new JButton("뒤로가기");
+		BackButton.setBackground(new Color(221, 160, 221));
+		BackButton.setForeground(Color.BLACK);
+		BackButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Test.card.show(Test.f,"main_screen");
+			}
+		});
+		BackButton.setBounds(14, 638, 149, 68);
+		p2.add(BackButton);
+		
+	}
+	
+	
+
+	public void sort(int l, int r) {
+        int left = l;
+        int right = r;
+        Menu pivot = MakeMenu.menu[(l + r) / 2]; // pivot 가운데 설정 (최악의 경우 방지)
+ 
+        do {
+            while (MakeMenu.menu[left].sold > pivot.sold)
+                left++;
+            while (MakeMenu.menu[right].sold < pivot.sold)
+                right--;
+ 
+            if (left <= right) {
+            		Menu temp = MakeMenu.menu[left];
+                    MakeMenu.menu[left] = MakeMenu.menu[right];
+                    MakeMenu.menu[right] = temp;
+                    left++;
+                    right--;
+                	
+            }
+        } while (left <= right);
+ 
+        if (l < right) {
+            sort( l, right);
+        }
+        if (r > left) {
+            sort(left, r);
+ 
+        }
+    }
+}
+
