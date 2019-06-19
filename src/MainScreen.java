7d7c4c138b682580d2321a54def8d507d3e9a7f0
potatoyum <46017398+potatoyum@users.noreply.github.com>
2019-06-19 21:44:38 +0900

    메인 화면

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,6 +1,7 @@
 package restaurant;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
@@ -9,7 +10,13 @@ import java.util.Vector;
 import java.awt.event.ActionEvent;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+import java.awt.Color;
+import java.awt.Font;
 
 @SuppressWarnings("serial")
 public class MainScreen extends JFrame {
@@ -18,7 +25,7 @@ public class MainScreen extends JFrame {
 	private JTable table;
 	int to;	
 	public MainScreen(){
-		
+
 		Order order = new Order();
 			
 		p1 = new JPanel();
@@ -31,9 +38,11 @@ public class MainScreen extends JFrame {
 		
 	//JPanel panel = new JPanel();
 	//panel.setBounds(14, 12, 317, 382);
-	
-	
+		Color color = new Color(254,206,0);
+		Color color2 = new Color(235,229,217);
+		Color backcolor = new Color(245,245,245);
 		panel_4 = new JPanel();
+		panel_4.setBackground(backcolor);
 		panel_4.setLocation(0, 0);
 		panel_4.setSize(1024, 768);
 		p1.add(panel_4);
@@ -43,44 +52,54 @@ public class MainScreen extends JFrame {
 		DefaultTableModel model1 = new DefaultTableModel(name_count,0);
 	
 		table = new JTable(model1);
-			
-			
+		table.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		table.setBackground(backcolor);
+		table.setRowHeight(30);
+		
+		DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();	
+		TableColumnModel columnModel = table.getColumnModel();
+		TableColumn column0 = columnModel.getColumn(0); 
+	    TableColumn column1 = columnModel.getColumn(1);
+	    renderer.setHorizontalAlignment(JLabel.CENTER);
+	    column0.setCellRenderer(renderer);
+	    column1.setCellRenderer(renderer);
+	    
 		JButton payment = new JButton("결제");
+		payment.setFont(new Font("맑은 고딕", Font.BOLD, 15));
 		payment.setBounds(267, 594, 159, 75);
+		payment.setBackground(color);
 		payment.addActionListener(new ActionListener() {
+				
 			public void actionPerformed(ActionEvent arg0) {
 					order.save();
-					order.print();
+					order.print();					
 					model1.setNumRows(0);//화면지우기
 					
-					for(int i =0;i<12;i++) {
-						Order.count[i]=0;					
+					for(int i =0;i<13;i++) {
+						Order.count[i]=0;
+						
 					}
 				}
 			});
+		
 			panel_4.setLayout(null);
 			panel_4.add(payment);
 			
-		
-		
-		
-		JButton reservation = new JButton("예약/ 자리 확인");
-		reservation.setBounds(768, 594, 159, 75);
-		reservation.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		
-		
-		panel_4.add(reservation);
 		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		tabbedPane_1.setBackground(color);
 		tabbedPane_1.setBounds(399, 11, 583, 519);
 		panel_4.add(tabbedPane_1);
 		JPanel panel_2 = new JPanel();
+		
+		panel_2.setBackground(color2);
 		tabbedPane_1.addTab("주 메뉴", panel_2);
 		panel_2.setLayout(null);
-	
+		panel_2.setBackground(backcolor);
 		JButton button_0 = new JButton("불고기버거 2000원");
+		button_0.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		
+		button_0.setBackground(color);
 		Vector<Object> contents1 = new Vector<Object>();
 		button_0.setBounds(25, 37, 158, 98);
 		
@@ -112,8 +131,10 @@ public class MainScreen extends JFrame {
 		/////////////////////////////////////////////////////////////
 		Vector<Object> contents2 = new Vector<Object>();
 		JButton button_1 = new JButton("치킨버거 3000원");
+		button_1.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_1.setBackground(color);
 		button_1.addActionListener(new ActionListener() {
-			
+		
 			public void actionPerformed(ActionEvent arg0) {
 				
 						order.setCount(1);
@@ -143,6 +164,8 @@ public class MainScreen extends JFrame {
 		///////////////////////////////////////////////////////
 		Vector<Object> contents3 = new Vector<Object>();
 		JButton button_2 = new JButton("치즈버거 2000원");
+		button_2.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_2.setBackground(color);
 		button_2.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
@@ -171,6 +194,8 @@ public class MainScreen extends JFrame {
 		//////////////////////////////////////////////////
 		Vector<Object> contents4 = new Vector<Object>();
 		JButton button_3 = new JButton("통새우버거 4000원");
+		button_3.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_3.setBackground(color);
 		button_3.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
@@ -199,6 +224,8 @@ public class MainScreen extends JFrame {
 		////////////////////////////////////////////////
 		Vector<Object> contents5 = new Vector<Object>();
 		JButton button_4 = new JButton("한우버거 4500원");
+		button_4.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_4.setBackground(color);
 		button_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				order.setCount(4);
@@ -225,6 +252,8 @@ public class MainScreen extends JFrame {
 	//////////////////////////////////////////////////////////////	
 		Vector<Object> contents6 = new Vector<Object>();
 		JButton button_5 = new JButton("강정버거 4000원");
+		button_5.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_5.setBackground(color);
 		button_5.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
@@ -255,10 +284,12 @@ public class MainScreen extends JFrame {
 		JPanel panel_1 = new JPanel();
 		tabbedPane_1.addTab("사이드 메뉴", panel_1);
 		panel_1.setLayout(null);
-		
+		panel_1.setBackground(backcolor);
 		/////////////////////////////////////////////
 		Vector<Object> contents7 = new Vector<Object>();
 		JButton button_6 = new JButton("감자튀김 1500원");
+		button_6.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_6.setBackground(color);
 		button_6.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
@@ -286,6 +317,8 @@ public class MainScreen extends JFrame {
 		////////////////////////////////////////////////
 		Vector<Object> contents8 = new Vector<Object>();
 		JButton button_7 = new JButton("아이스크림 500원");
+		button_7.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_7.setBackground(color);
 		button_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				order.setCount(7);
@@ -312,6 +345,8 @@ public class MainScreen extends JFrame {
 		//////////////////////////////////////////////////////
 		Vector<Object> contents9 = new Vector<Object>();
 		JButton button_8 = new JButton("스낵랩 2500원");
+		button_8.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_8.setBackground(color);
 		button_8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				order.setCount(8);
@@ -341,9 +376,11 @@ public class MainScreen extends JFrame {
 		JPanel panel_3 = new JPanel();
 		tabbedPane_1.addTab("음료", panel_3);
 		panel_3.setLayout(null);
-		
+		panel_3.setBackground(backcolor);
 		Vector<Object> contents10 = new Vector<Object>();
 		JButton button_9 = new JButton("콜라 1000원");
+		button_9.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_9.setBackground(color);
 		button_9.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
@@ -372,6 +409,8 @@ public class MainScreen extends JFrame {
 		///////////////////////////////////////////////////
 		Vector<Object> contents11 = new Vector<Object>();
 		JButton button_10 = new JButton("사이다 1000원");
+		button_10.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_10.setBackground(color);
 		button_10.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
@@ -401,6 +440,8 @@ public class MainScreen extends JFrame {
 		////////////////////////////////////////////////
 		Vector<Object> contents12 = new Vector<Object>();
 		JButton button_11 = new JButton("환타 1000원");
+		button_11.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_11.setBackground(color);
 		button_11.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				order.setCount(11);
@@ -427,6 +468,8 @@ public class MainScreen extends JFrame {
 		////////////////////////////////////////
 		Vector<Object> contents13 = new Vector<Object>();
 		JButton button_12 = new JButton("마운틴듀 1000원");
+		button_12.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		button_12.setBackground(color);
 		button_12.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				order.setCount(12);
@@ -452,7 +495,9 @@ public class MainScreen extends JFrame {
 		panel_3.add(button_12);
 		
 		JScrollPane tablePane = new JScrollPane(table);
-		tablePane.setBounds(0, 12, 385, 518);
+		tablePane.setBounds(12, 12, 373, 518);
+		tablePane.setBackground(color);
 		panel_4.add(tablePane);
+		panel_4.setBackground(backcolor);
 }
 }
