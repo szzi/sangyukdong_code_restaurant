86c018f71e90f07681cc435d75366c715d8c333f
potatoyum <46017398+potatoyum@users.noreply.github.com>
2019-06-19 21:59:24 +0900

    영수증 출력
    영수증 출력

--- src/Receipt.java
+++ src/Receipt.java
@@ -5,19 +5,22 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class Receipt extends JFrame {
 
-	/**
-	 * 
-	 */
+	
 	//private static final long serialVersionUID = 8154583553222364300L;
 	private JPanel contentPane;
 	public int total, totalsum=0;	
 	
 	Receipt() {
+		setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 12));
+		getContentPane().setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 12));
 	}
 	
 	public void run() {
@@ -25,7 +28,7 @@ public class Receipt extends JFrame {
 		setSize(450,600);
 		setVisible(true);
 		setBackground(Color.WHITE);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//sub_f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
@@ -33,10 +36,10 @@ public class Receipt extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		//±âº» ·¹ÀÌ¾Æ¿ô
-		JLabel lblNewLabel = new JLabel("»ê°İµ¿ ÇÜ¹ö°Å");
+		//ê¸°ë³¸ ë ˆì´ì•„ì›ƒ
+		JLabel lblNewLabel = new JLabel("ì‚°ê²©ë™ í–„ë²„ê±°");
 		lblNewLabel.setBounds(160, 8, 132, 36);
-		lblNewLabel.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblNewLabel.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 20));
 		contentPane.add(lblNewLabel);
 		
 		JLabel label_1 = new JLabel("----------------------------------------------------------------------------------------------------");
@@ -48,17 +51,17 @@ public class Receipt extends JFrame {
 		contentPane.add(label_2);
 		
 		JLabel lblMenu = new JLabel("Name");
-		lblMenu.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblMenu.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 18));
 		lblMenu.setBounds(22, 79, 50, 20);
 		contentPane.add(lblMenu);
 		
 		JLabel lblSum = new JLabel("Qty");
-		lblSum.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblSum.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 20));
 		lblSum.setBounds(220, 79, 50, 20);
 		contentPane.add(lblSum);
 		
 		JLabel lblCost = new JLabel("Total");
-		lblCost.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblCost.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 20));
 		lblCost.setBounds(351, 79, 50, 20);
 		contentPane.add(lblCost);
 		
@@ -67,30 +70,39 @@ public class Receipt extends JFrame {
 		contentPane.add(label_3);
 		
 		JLabel lblTotal = new JLabel("Total");
-		lblTotal.setFont(new Font("±¼¸²", Font.PLAIN, 18));
+		lblTotal.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 18));
 		lblTotal.setBounds(30, 513, 50, 15);
 		contentPane.add(lblTotal);
 		
 		
-		//ÁÖ¹®³»¿ëÃâ·Â
+		for(int i=0;i<13;i++) {
+			
+				System.out.printf("%s/",MakeMenu.menuName[i]);
+				
+			}
+		System.out.printf("\n");
+		
+		//ì£¼ë¬¸ë‚´ìš©ì¶œë ¥
 		//ordername
 		JTextArea ordername = new JTextArea();
+		ordername.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 12));
 		ordername.setText("");
 		ordername.setEditable(false);
 		for(int i=0;i<13;i++)
-			if(Order.count[i]!=0) {
-				ordername.append(String.format("%s\n",MakeMenu.menu[i].name));
+			if(Order.pre_count[i]!=0) {
+				ordername.append(String.format("%s\n",MakeMenu.menuName[i]));
 				contentPane.add(ordername);
 			}
 		ordername.setBounds(22, 141, 60, 324);
 				
 		//ordersum
 		JTextArea ordersum = new JTextArea();
+		ordername.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 12));
 		ordersum.setText("");
 		ordersum.setEditable(false);
 		for(int i=0;i<13;i++)
-			if(Order.count[i]!=0) {
-				ordersum.append(String.format("%d\n",MakeMenu.menu[i].sold));
+			if(Order.pre_count[i]!=0) {
+				ordersum.append(String.format("%d\n",Order.pre_count[i]));
 				contentPane.add(ordersum);
 			}
 		ordersum.setBounds(235, 141, 30, 324);
@@ -100,10 +112,10 @@ public class Receipt extends JFrame {
 		JTextArea ordercost = new JTextArea();
 		ordercost.setText("");
 		ordercost.setEditable(false);
-		
+		ordercost.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 12));
 		for(int i=0;i<13;i++)
-			if(Order.count[i]!=0) {
-				total=MakeMenu.menu[i].cost*Order.count[i];
+			if(Order.pre_count[i]!=0) {
+				total=MakeMenu.menuCost[i]*Order.pre_count[i];
 				ordercost.append(String.format("%d\n",total));
 				contentPane.add(ordercost);
 				totalsum+=total;
@@ -112,11 +124,21 @@ public class Receipt extends JFrame {
 		
 		//total
 		JTextArea ordertotal = new JTextArea();
+		ordertotal.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 12));
 		ordertotal.setText("");
 		ordertotal.setEditable(false);
 		ordertotal.append(String.format("%d", totalsum));
 		contentPane.add(ordertotal);
 		ordertotal.setBounds(350, 509, 79, 24);
+		
+		
+		addWindowListener(new WindowAdapter(){
+			   public void windowClosing (WindowEvent  e) {
+				  setVisible(false);
+				   dispose();
+				  }
+			  });
+			
 	}
 	
 }
